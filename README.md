26e8ff29b34a7c18ba1d29d57abb35d5ed555b1b
David Lanner <dhlanner2@gmail.com>
2016-03-31 17:32:30 -0400

    Initial commit; pidenv

---
+++ README.md
@@ -0,0 +1,11 @@
+To use these functions, run:
+
+```
+./init.sh
+```
+
+To use immediately in the current terminal session:
+
+```
+source ~/.bash_profile
+```
---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DIRECTORY=~/.bash_functions
+if [ ! -d "$DIRECTORY" ];
+then
+  mkdir $DIRECTORY
+fi
+
+cp -R ./functions/ $DIRECTORY/
+
+tee -a ~/.bash_profile <<- 'EOF'
+if [ -d ~/.bash_functions ]; then
+  for file in ~/.bash_functions/*; do
+    . "$file"
+  done
+fi
+EOF
